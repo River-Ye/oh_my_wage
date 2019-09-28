d92a143ef20b7c616d741203a58c8b22768d3fa4
Patrick Liu <kazupat0906@gmail.com>
2019-09-28 13:00:35 +0800

    homepage

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,11 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
+<<<<<<< Updated upstream
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+=======
+  config.mailer_sender = '霍格華滋學院'
+>>>>>>> Stashed changes
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
