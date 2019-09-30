528fcc534dde9a1f0c6832f45fc2e6acc159bb3a
River <river800111@gmail.com>
2019-09-30 23:37:28 +0800

    Add safe

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :timeoutable, :timeout_in => 10.minutes
 
   paginates_per 10
 
