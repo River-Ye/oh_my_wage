9fb708f7a6f9d617067f86067dc5937a01749b49
David Chen <davidchen1994520@gmail.com>
2019-10-04 11:14:02 +0800

    pdf ok

--- app/controllers/staff/salaries_controller.rb
+++ app/controllers/staff/salaries_controller.rb
@@ -15,31 +15,18 @@ class Staff::SalariesController < ApplicationController
       user_id = DepartmentWithUser.where(department_id: @department_id).map{|x| x.user_id}
       # 找出該部門下的學生
       @students = User.where(id: user_id).where(role: 2).search(params[:search]).order(name: :asc).page(params[:page])
-      respond_to do |format|
-        format.html
-        format.json
-        format.pdf { render template: 'users/pdf',pdf:'pdf' }
-      end
+      
     end
   end
 
   def show
-  end
-  
-  def pdf
-    render html: params(@salary)
-    if DepartmentWithUser.find_by(user_id: current_user.id).nil?
-      redirect_to root_path, notice: "不隸屬任何部門喔，請向管理者反映!!"
-    else
-      # 找出登入老師的所屬部門代號
-      @department_id = DepartmentWithUser.find_by(user_id: current_user.id)[:department_id]
-      # 找出該部門底下所有人
-      user_id = DepartmentWithUser.where(department_id: @department_id).map{|x| x.user_id}
-      # 找出該部門下的學生
-      @students = User.where(id: user_id).where(role: 2).order(name: :asc).page(params[:page])
+    respond_to do |format|
+      format.html
+      format.json
+      format.pdf{ render template:'staff/salaries/pdf',pdf:'pdf' }
     end
   end
-
+  
 
   def edit
     @student.salaries.build if @student.salaries.empty?
