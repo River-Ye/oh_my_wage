7e98edb48523d6f3a349d4fb656687c3558d67f6
River <river800111@gmail.com>
2019-10-04 23:53:36 +0800

    Fix problems_controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_10_04_075432) do
-
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,15 +73,12 @@ ActiveRecord::Schema.define(version: 2019_10_04_075432) do
     t.integer "role", default: 2
     t.string "phone"
     t.integer "gender", default: 0
-
+    t.string "slug"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-
-    t.string "slug"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
