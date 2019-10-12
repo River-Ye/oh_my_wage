d132d45638bfaad4ff0db908295c3512dcf6bd27
David Chen <davidchen1994520@gmail.com>
2019-10-12 23:06:12 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
     resources 'users', only: [:index, :show, :update]
     resources 'salaries', except: [:new, :create] do
       collection  do
-        get :pdf
+        get :pdf 
       end  
     end   
     get 'history', to: 'user#history'
