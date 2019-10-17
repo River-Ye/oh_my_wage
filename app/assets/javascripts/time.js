f81115f542c603dbe78d226e2909c4ca1fb57bab
joanliao0525 <comeon19940525@hotmail.com>
2019-10-17 18:02:39 +0800

    fix timecount safari

--- app/assets/javascripts/time.js
+++ app/assets/javascripts/time.js
@@ -4,27 +4,35 @@ window.onload = function() {console.log
 
 function startTimer () {
     var logout_value = document.querySelector('#logout-timestamp')
+    // var logout_value = $('#logout-timestamp')
+    var deadline = Date.parse( new Date(logout_value.dataset.logoutTime.replace(/-/g, '/')))
+    // console.log('deadline1', deadline);
 
-    var deadline = new Date(logout_value.dataset.logoutTime).getTime();
- 
-    console.log('deadline', deadline);
+    // var deadline = new Date(logout_value.dataset.logoutTime).getTime();
+    // console.log('deadline2', deadline);
+    //  debugger
 
     var x = setInterval(function() { 
-    var now = new Date().getTime();
-    var t = deadline - now; 
-    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
-    var seconds = Math.floor((t % (1000 * 60)) / 1000); 
+        var now = new Date().getTime();
+        var t = deadline - now; 
+        // debugger
+        // console.log('deadline: ', deadline)
+        // console.log('now: ', now)
+        // console.log('t: ', t)
+        var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
+        var seconds = Math.floor((t % (1000 * 60)) / 1000); 
 
-    console.log('minutes', minutes)
-    console.log('seconds', seconds)
-    document.getElementById("timecount").innerHTML = minutes + "m " + seconds + "s "; 
-    
+        // console.log('2minutes', minutes)
+        // console.log('seconds', seconds)
+        // // document.getElementById("timecount").innerHTML = minutes + "m " + seconds + "s "; 
+        $('#timecount').html( minutes + "m " + seconds + "s ")
+        
 
-    if (seconds < 0) { 
-      clearInterval(x); 
-      document.getElementById("timecount").innerHTML = "時間到";
-    } 
-  
-}, 1000);
+        if (seconds < 0) { 
+          clearInterval(x); 
+          document.getElementById("timecount").innerHTML = "時間到";
+        } 
+      
+    }, 1000);
   
 }
